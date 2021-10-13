aae6100514bd25096255e0354158920bcb5c5765
adey-px <aosteph@gmail.com>
2021-10-13 22:26:47 +0000

    Setup All Authentication (allauth)

--- cluster/settings.py
+++ cluster/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites', # From this line copied from allauth site
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'home',
 ]
 
@@ -67,6 +72,29 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+# For sending confirmation email links to new accounts
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Additional settings for allauth to work
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+AACOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGHT = 4
+LOGIN_URL = '/accounts/login'  # Specify login url
+LOGIN_REDIRECT_URL = '/'  # Redirect to site homepage after login
 
 WSGI_APPLICATION = 'cluster.wsgi.application'
 
