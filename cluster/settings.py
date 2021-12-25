1abbd92d8bf1d7002d22b38d570abb681faa6bf0
adey-px <aosteph@gmail.com>
2021-12-25 03:17:46 +0000

    Added secret key in setting

--- cluster/settings.py
+++ cluster/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 # ORIGINALLY INSTALLED WITH 3.2.8
 # CURRENT DJANGO VERSION IS 3.0.1
 
-from secret import *
 import os
 from pathlib import Path
 import dj_database_url
@@ -25,6 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'xyz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
